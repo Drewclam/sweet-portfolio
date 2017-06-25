725d5dbc163ea6de2439e23399d5b231e6981cad
Drewclam <andrew.c.lam94@gmail.com>
2017-06-24 23:22:51 -0400

    Fix picture url

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import Navbar from './components/Navbar';
 
 class App extends Component {
   render() {
+    const displayPictureUrl = 'https://scontent.fybz2-2.fna.fbcdn.net/v/t1.0-9/19420494_10212759759449398_1377538664024453148_n.jpg?oh=b52009b23233708b36e17bde5c492b50&oe=59D8BBA5';
     return (
       <div className="App">
-        <Navbar />
+        <Navbar url={displayPictureUrl} />
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Navbar extends Component {
   render() {
     return (
-      <h1>Header!</h1>
+      <img src={this.props.url} />
     );
   }
 
