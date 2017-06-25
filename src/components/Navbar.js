22c13e8ff09bc34d0636219bae041b0d95b6f8e4
Drewclam <andrew.c.lam94@gmail.com>
2017-06-24 23:45:50 -0400

    Add links to icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
 class Navbar extends Component {
+  openFacebook() {
+    window.open()
+  }
+
   render() {
     return (
       <div className="navbar">
@@ -9,7 +13,15 @@ class Navbar extends Component {
         <span>Projects</span>
         <span>Contact</span>
         <div className="social-media">
-          <i className="fa fa-facebook-square fa-3x" aria-hidden="true"></i>
+          <a href="https://www.facebook.com/DrewCLam">
+            <i className="fa fa-facebook-square fa-3x" aria-hidden="true"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/drewclam">
+            <i className="fa fa-linkedin-square fa-3x" aria-hidden="true"></i>
+          </a>
+          <a href="https://github.com/drewclam">
+            <i className="fa fa-github-square fa-3x" aria-hidden="true"></i>
+          </a>
         </div>
       </div>
     );
