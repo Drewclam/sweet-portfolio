71faf542f6c89fffdfedbc692a32c1b20b2c6784
Drewclam <andrew.c.lam94@gmail.com>
2017-06-26 00:32:12 -0400

    Add take a hike project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -20,6 +20,25 @@ export default class Projects extends Component {
           <span><h4>Notable Technologies</h4>Built with React Native, Postgres, Node.js, Express.js, Expo</span>
           <div>
             <img src="https://puu.sh/wupPV/83885fc8cf.png" />
+            <img src="https://puu.sh/wupYC/73a98aae4a.png" />
+          </div>
+        </div>
+        <div>
+          <h3>FindAHike</h3>
+          <span><h4>Summary</h4>A web-app powered by the Trail API. Discover hiking trails near you, track completed hikes or save them on a wishlist for later.</span>
+          <span><h4>Notable Technologies</h4>Built with AngularJS, MySQL, Sequelize Node.js, Express.js, Bootstrap, Trail API, Google Maps API</span>
+          <div>
+            <img src="https://puu.sh/wuqvb/e8ca156dd4.png" />
+            <img src="https://puu.sh/wuqxK/846b3e0b54.png" />
+          </div>
+        </div>
+        <div>
+          <h3>FindAHike</h3>
+          <span><h4>Summary</h4>A web-app powered by the Trail API. Discover hiking trails near you, track completed hikes or save them on a wishlist for later.</span>
+          <span><h4>Notable Technologies</h4>Built with AngularJS, MySQL, Sequelize Node.js, Express.js, Bootstrap, Trail API, Google Maps API</span>
+          <div>
+            <img src="" />
+            <img src="" />
           </div>
         </div>
       </div>
