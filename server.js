230009ee0adc94e2c3d8071b8539c1b05273c707
Drewclam <andrew.c.lam94@gmail.com>
2017-06-26 15:03:23 -0400

    Add transporter

--- server.js
+++ server.js
@@ -2,7 +2,18 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 const app = express();
 const port = 1337;
+const account = require('./config.js');
 
 app.use(express.static(__dirname + '/build'));
+
+app.post('/contact', (req, res) => {
+  const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: account.user,
+      pass: account.pass
+    }
+  });
+});
 
 app.listen(port, () => console.log('App listening on 1337.'));
