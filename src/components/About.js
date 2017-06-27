67be318e465fab6ecb1b36708a51b51f64969b60
Drewclam <andrew.c.lam94@gmail.com>
2017-06-27 03:14:40 -0400

    Add flex to header and about

--- src/components/About.js
+++ src/components/About.js
@@ -3,13 +3,15 @@ import React, { Component } from 'react';
 export default class About extends Component {
   render() {
     return (
-      <div>
+      <div className="about-container">
         <a name="about"></a>
-        <h1>About</h1>
         <img className="profile-pic" src={this.props.url} />
-        <p>
-          My name is Andrew Lam. I am a full-stack JavaScript web developer who loves working with Angular 4, AngularJS, React and React Native front-end frameworks. I am experienced with Node.js and Express.js backend technologies and MongoDB, MySQL and Postgres databases. If you are looking for a talented and dedicated JavaScript engineer, reach out!
-        </p>
+        <div className="about-text-container">
+          <h1 className="about-header">About</h1>
+          <p className="about-blurb">
+            My name is Andrew Lam. I am a full-stack JavaScript web developer who loves working with Angular 4, AngularJS, React and React Native front-end frameworks. I am experienced with Node.js and Express.js backend technologies and MongoDB, MySQL and Postgres databases. If you are looking for a talented and dedicated JavaScript engineer, reach out!
+          </p>
+        </div>
       </div>
     );
   }
