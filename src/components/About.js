8c7a061a2629739327d1cfa9abd1818f6198aaf8
Drewclam <andrew.c.lam94@gmail.com>
2017-06-27 19:25:49 -0400

    Remove techs from about and center image and about text

--- src/components/About.js
+++ src/components/About.js
@@ -7,22 +7,19 @@ export default class About extends Component {
     return (
       <div className="about-container">
         <a name="about"></a>
-        <h1 className="about-header">About</h1>
         <img className="profile-pic" src={picture} />
-        <div className="about-info-container">
           <p className="about-blurb">
             My name is Andrew Lam. I am a full-stack JavaScript web developer who loves working with Angular 4, AngularJS, React and React Native front-end frameworks. I am experienced with Node.js and Express.js backend technologies and MongoDB, MySQL and Postgres databases. If you are looking for a talented and dedicated JavaScript engineer, reach out!
           </p>
-          <div className="about-tech-container">
-            <li className="about-tech">React.js</li>
-            <li className="about-tech">Angular 4</li>
-            <li className="about-tech">AngularJS</li>
-            <li className="about-tech">Node.js + Express.js</li>
-            <li className="about-tech">MySQL</li>
-            <li className="about-tech">MongoDB</li>
-          </div>
-        </div>
       </div>
     );
   }
 }
+          // <div className="about-tech-container">
+          //   <li className="about-tech">React.js</li>
+          //   <li className="about-tech">Angular 4</li>
+          //   <li className="about-tech">AngularJS</li>
+          //   <li className="about-tech">Node.js + Express.js</li>
+          //   <li className="about-tech">MySQL</li>
+          //   <li className="about-tech">MongoDB</li>
+          // </div>
