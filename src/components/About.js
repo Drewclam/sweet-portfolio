c5833434f4ede5f4c483257b3a1d8532b993ec69
Drewclam <andrew.c.lam94@gmail.com>
2017-06-28 16:21:12 -0400

    Fix about css

--- src/components/About.js
+++ src/components/About.js
@@ -7,6 +7,7 @@ export default class About extends Component {
     return (
       <div className="about-container">
         <a name="about"></a>
+        <span>Tech</span>
         <img className="profile-pic" src={picture} />
         <div className="about-blurb-container">
           <h1>Hello.</h1><p>Hi! My name is Andrew. I'm a full-stack JavaScript software engineer. Ever since I was a kid I loved playing video games. Developing this passion into a programming career was a natural transition for me. Even now I'm still an avid gamer (ranked top 1% in League of Legends)! Recently, I've collaborated on projects using frameworks such as Angular 4, AngularJS, React and React Native. On the back-end I'm experienced with using Node.js, Express.js, MySQL and MongoDB. If you are looking for a talented and dedicated JavaScript engineer, <a href="#contact">reach out!</a></p>
