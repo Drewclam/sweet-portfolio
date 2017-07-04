96f13bf399df7ae01c1a8515be8f442491162e16
Drewclam <andrew.c.lam94@gmail.com>
2017-07-03 23:52:11 -0400

    Fix social media icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
+import { SocialIcon } from '../../node_modules/react-social-icons/lib/react-social-icons.js';
 
 export default class Header extends Component {
   render() {
+    const socialUrls = [
+      'https://www.facebook.com/DrewCLam',
+      'https://www.linkedin.com/in/drewclam',
+      'https://github.com/drewclam'
+    ];
+
     return (
       <div className="header-container">
         <a name="about"></a>
         <span className="header-name">Andrew Lam</span>
         <div className="social-media-container">
-          <a href="https://www.facebook.com/DrewCLam">
-            <i className="fa fa-facebook" aria-hidden="true"></i>
-          </a>
-          <a href="https://www.linkedin.com/in/drewclam">
-            <i className="fa fa-linkedin" aria-hidden="true"></i>
-          </a>
-          <a href="https://github.com/drewclam">
-            <i className="fa fa-github-alt" aria-hidden="true"></i>
-          </a>
+          {socialUrls.map(url =>
+            <div className="social-media-item">
+            <SocialIcon
+              url={url}
+              color="black"
+              style={{ height: 35, width: 35 }}
+            />
+            </div>
+          )}
         </div>
       </div>
     );
