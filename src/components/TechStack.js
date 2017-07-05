25934f6e6e8d288758154174a787d270d04e99ca
Drewclam <andrew.c.lam94@gmail.com>
2017-07-05 02:38:06 -0400

    Replace icons with words

--- src/components/TechStack.js
+++ src/components/TechStack.js
@@ -6,26 +6,44 @@ export default class TechStack extends Component {
       <div className="tech-container">
         <a name="tech"></a>
         <h1>Tech Stack</h1>
-        <div className="tech-icon-wrapper">
-          <i className="icon-tech-javascript"></i>
-          <i className="icon-tech-typescript"></i>
-          <i className="icon-tech-react"></i>
-          <i className="icon-tech-angular"></i>
-          <i className="icon-tech-backbone"></i>
-          <i className="icon-tech-nodejs"></i>
-          <i className="icon-tech-expressjs"></i>
-          <i className="icon-tech-mongo"></i>
-          <i className="icon-tech-mysql"></i>
-          <i className="icon-tech-postgres"></i>
-          <i className="icon-tech-jquery"></i>
-          <i className="icon-tech-grunt"></i>
-          <i className="icon-tech-heroku"></i>
-          <em className="icon-github"></em>
-          <em className="icon-html5 add-size"></em>
-          <em className="icon-css3-alt"></em>
-          <i className="icon-tech-bootstrap"></i>
+        <div className="tech-wrapper">
+          <p className="small">Html.</p>
+          <p className="large">Angular 2.</p>
+          <p className="med">React.</p>
+          <p className="small">Grunt.</p>
+          <p className="small">TypeScript.</p>
+          <p className="small">Backbone.</p>
+          <p className="small">NodeJS.</p>
+          <p className="small">ExpressJS.</p>
+          <p className="small">MongoDB.</p>
+          <p className="small">MySQL.</p>
+          <p className="small">PostgreSQL.</p>
+          <p className="small">Jquery.</p>
+          <p className="small">Heroku.</p>
+          <p className="small">Git.</p>
+          <p className="small">Grunt.</p>
+          <p className="small">CSS3.</p>
         </div>
       </div>
     );
   }
 }
+        // <div className="tech-icon-wrapper">
+          // <i className="icon-tech-javascript"></i>
+          // <i className="icon-tech-typescript"></i>
+          // <i className="icon-tech-react"></i>
+          // <i className="icon-tech-angular"></i>
+          // <i className="icon-tech-backbone"></i>
+          // <i className="icon-tech-nodejs"></i>
+          // <i className="icon-tech-expressjs"></i>
+          // <i className="icon-tech-mongo"></i>
+          // <i className="icon-tech-mysql"></i>
+          // <i className="icon-tech-postgres"></i>
+          // <i className="icon-tech-jquery"></i>
+          // <i className="icon-tech-grunt"></i>
+          // <i className="icon-tech-heroku"></i>
+          // <em className="icon-github"></em>
+          // <em className="icon-html5 add-size"></em>
+          // <em className="icon-css3-alt"></em>
+          // <i className="icon-tech-bootstrap"></i>
+        // </div>
