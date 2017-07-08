5385d528b51bd1ad48b38d5deac09b288cd13453
Drewclam <andrew.c.lam94@gmail.com>
2017-07-08 09:40:06 -0400

    Fix positioning
    Still need to fix top right project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,21 +11,21 @@ export default class Projects extends Component {
         <a name="projects"></a>
         <p className="section-title">Projects.</p>
         <div className="projects-wrapper">
-          <div className="project" onClick={() => renderModal(0)}>
-            <h3>It's Cut Time</h3>
-            <img src="https://puu.sh/wuo5m/5a47b0c5a4.png" alt="" />
+          <div className="project project-left" onClick={() => renderModal(0)}>
+            <span className="project-title">It's Cut Time</span>
+            <img className="project-thumb" src="https://puu.sh/wuo5m/5a47b0c5a4.png" alt="" />
           </div>
-          <div className="project" onClick={() => renderModal(1)}>
-            <h3>Recipely</h3>
-            <img src={recipely} alt="" />
+          <div className="project project-right" onClick={() => renderModal(1)}>
+            <span className="project-title">Recipely</span>
+            <img className="project-thumb" src={recipely} alt="" />
           </div>
-          <div className="project" onClick={() => renderModal(2)}>
-            <h3>FindAHike</h3>
-            <img src="https://puu.sh/wuqvb/e8ca156dd4.png" alt="" />
+          <div className="project project-left" onClick={() => renderModal(2)}>
+            <span className="project-title">FindAHike</span>
+            <img className="project-thumb" src="https://puu.sh/wuqvb/e8ca156dd4.png" alt="" />
           </div>
-          <div className="project" onClick={() => renderModal(3)}>
-            <h3>ng-Challenge</h3>
-            <img src="https://puu.sh/wniMK/5730fe753f.png" alt="" />
+          <div className="project project-right" onClick={() => renderModal(3)}>
+            <span className="project-title">ng-Challenge</span>
+            <img className="project-thumb" src="https://puu.sh/wniMK/5730fe753f.png" alt="" />
           </div>
         </div>
         <ProjectInfo
