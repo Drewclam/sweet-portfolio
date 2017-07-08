ff99bb0220b2975c663124c45bb99804dfa91f44
Drewclam <andrew.c.lam94@gmail.com>
2017-07-08 05:47:32 -0400

    Remove message send gif and add required on inputs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import checkmark from '../images/checkmark.gif';
 
 export default class Contact extends Component {
   render() {
@@ -8,18 +7,20 @@ export default class Contact extends Component {
       <div className="contact-container">
         <a name="contact"></a>
         <p className="section-title">Contact.</p>
-        <div className="contact-message-wrapper">
+        <form className="contact-message-wrapper">
           <div className="contact-message-meta">
             <input
               className="contact-message-name"
               value={name}
               placeholder="Name"
+              required
               onChange={e => changeName(e.target.value)}
             />
             <input
               className="contact-message-email"
               value={email}
               placeholder="Email"
+              required
               onChange={e => changeEmail(e.target.value)}
             />
           </div>
@@ -27,13 +28,24 @@ export default class Contact extends Component {
             className="contact-message-body"
             placeholder="type a message..."
             value={body}
+            required
             onChange={e => changeBody(e.target.value)}
           >
           </textarea>
-          <div className="contact-message-submit-wrapper" onClick={sendMessage}>
-            {sent ? <div className="contact-message-submit-confirmation"><img src={checkmark} />Sent</div> : <div className="contact-message-submit-btn">Submit</div>}
+          <div className="contact-message-submit-wrapper">
+            {sent ?
+              <div className="contact-message-submit-confirmation">
+                Sent. Thank yoasdfu!
+              </div> :
+              <input
+                className="contact-message-submit-btn"
+                type="submit"
+                placeholder="asdf"
+              />
+
+              }
           </div>
-        </div>
+        </form>
       </div>
     );
   }
