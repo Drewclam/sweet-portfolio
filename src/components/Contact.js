88ca319034236c37462909cbc689ef9a243cc77e
Drewclam <andrew.c.lam94@gmail.com>
2017-07-07 20:33:33 -0400

    Add positioning for contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,23 +7,29 @@ export default class Contact extends Component {
       <div className="contact-container">
         <a name="contact"></a>
         <p className="section-title">Contact.</p>
-        <input
-          value={name}
-          placeholder="Name"
-          onChange={e => changeName(e.target.value)}
-        />
-        <input
-          value={email}
-          placeholder="Email"
-          onChange={e => changeEmail(e.target.value)}
-        />
-        <textarea
-          placeholder="Type a message..."
-          value={body}
-          onChange={e => changeBody(e.target.value)}
-        >
-        </textarea>
-        <div onClick={sendMessage}>Submit</div>
+        <div className="contact-message-wrapper">
+          <div className="contact-message-meta">
+            <input
+              className="contact-message-name"
+              value={name}
+              placeholder="Name"
+              onChange={e => changeName(e.target.value)}
+            />
+            <input
+              className="contact-message-email"
+              value={email}
+              placeholder="Email"
+              onChange={e => changeEmail(e.target.value)}
+            />
+          </div>
+          <textarea
+            placeholder="Type a message..."
+            value={body}
+            onChange={e => changeBody(e.target.value)}
+          >
+          </textarea>
+          <div onClick={sendMessage}>Submit</div>
+        </div>
       </div>
     );
   }
