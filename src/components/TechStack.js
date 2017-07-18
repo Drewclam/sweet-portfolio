cf4730c255955b54dc808832947528cac62538a1
Drewclam <andrew.c.lam94@gmail.com>
2017-07-17 23:13:51 -0400

    Add new design for tech stack

--- src/components/TechStack.js
+++ src/components/TechStack.js
@@ -3,34 +3,35 @@ import React, { Component } from 'react';
 export default class TechStack extends Component {
   render() {
     return (
-      <div className="tech-container">
-        <a name="tech"></a>
-        <p className="section-title alt-section-title">Tech Stack.</p>
-        <div className="tech-wrapper">
-          <p className="small">Html.</p>
-          <p className="large">Angular 2.</p>
-          <p className="large">React.</p>
-          <p className="small">Grunt.</p>
-          <p className="large">TypeScript.</p>
-          <p className="med">Backbone.</p>
-          <p className="large">NodeJS.</p>
-          <p className="med">PostgreSQL.</p>
-          <p className="med">CSS3.</p>
-          <p className="large">ExpressJS.</p>
-          <p className="small">Bootstrap.</p>
-          <p className="med">MongoDB.</p>
-          <p className="med">Heroku.</p>
-          <p className="large">MySQL.</p>
-          <p className="large">Jquery.</p>
-          <p className="med">Digital Ocean.</p>
-          <p className="large">Git.</p>
-          <p className="med">React Native.</p>
-          <p className="med">Auth0.</p>
-          <p className="small">D3.</p>
-          <p className="large">AngularJS.</p>
-          <p className="med">Mocha/Chai.</p>
+      <a name="tech">
+        <div className="tech-container">
+          <p className="section-title tech-section-title">Tech Stack.</p>
+          <div className="tech-wrapper">
+            <p className="small">Html.</p>
+            <p className="large">Angular 2.</p>
+            <p className="small">Grunt.</p>
+            <p className="small">TypeScript.</p>
+            <p className="small">Backbone.</p>
+            <p className="small">NodeJS.</p>
+            <p className="small">PostgreSQL.</p>
+            <p className="small">CSS3.</p>
+            <p className="large">React.</p>
+            <p className="small">ExpressJS.</p>
+            <p className="small">Bootstrap.</p>
+            <p className="large">MongoDB.</p>
+            <p className="small">Heroku.</p>
+            <p className="small">MySQL.</p>
+            <p className="small">Jquery.</p>
+            <p className="small">Digital Ocean.</p>
+            <p className="large">Git.</p>
+            <p className="small">React Native.</p>
+            <p className="small">Auth0.</p>
+            <p className="small">D3.</p>
+            <p className="large">AngularJS.</p>
+            <p className="small">Mocha/Chai.</p>
+          </div>
         </div>
-      </div>
+      </a>
     );
   }
 }
